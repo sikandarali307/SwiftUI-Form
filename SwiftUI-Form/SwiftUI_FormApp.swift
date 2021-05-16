03810ee91e25c1e1cef7eb9f27c009c9ba9cf4b0
sikandar <sikandar.ali307@gmail.com>
2021-05-16 10:07:58 +0500

    Initial Commit

---
+++ SwiftUI-Form/SwiftUI_FormApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_FormApp.swift
+//  SwiftUI-Form
+//
+//  Created by Sikandar Ali on 16/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_FormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
